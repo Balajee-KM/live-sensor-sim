8916191f9f59b6942ca600d06a63ce0b7493f50e
Balajee <itsbalajeee@gmail.com>
2025-06-04 10:51:22 +0200

    Initial commit

---
+++ backend-fastapi/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow everyone to access your API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+data_store = []
+
+@app.post("/data")
+def receive_data(payload: dict):
+    data_store.append(payload)
+    return {"message": "Got it!"}
+
+@app.get("/data")
+def get_data():
+    return data_store[-10:]
